f912e2b8608fa98c2c20d44016d257e6c0084a2e
Mathichai Dechdee <44646767+mdechdee@users.noreply.github.com>
2025-02-08 21:16:48 +0000

    change room to store peerId

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from 'uuid';
 import { WebSocket, WebSocketServer } from 'ws';
 
 type WsMessage =
@@ -23,7 +22,6 @@ interface CandidateData extends RtcData {
 
 interface HostRoomMessage {
   type: "host-room";
-  roomId: string;
 }
 interface JoinRoomMessage {
   type: "join-room";
@@ -50,12 +48,13 @@ type RoomId = string;
 
 interface Room {
   host: WebSocket;
-  guests: WebSocket[];
-  offers: Map<WebSocket, OfferData>;
-  answers: Map<WebSocket, AnswerData>;
-  candidates: Map<WebSocket, CandidateData>;
+  peers: Map<number, {
+    ws: WebSocket;
+    offers: Map<WebSocket, OfferData>;
+    answers: Map<WebSocket, AnswerData>;
+    candidates: Map<WebSocket, CandidateData>;    
+  }>
 }
-
 
 const wss = new WebSocketServer({ port: 8080 })
 const rooms: Record<RoomId, Room> = {};
@@ -64,31 +63,31 @@ wss.on("connection", (ws) => {
   ws.on("message", (message) => {
     const data = JSON.parse(message.toString()) as WsMessage;
 
-    switch (data.type) {
-      case "host-room":
-        const roomId = Math.random().toString(36).substr(2, 5);
-        rooms[roomId] = {
-          host: ws,
-          guests: [],
-          offers: new Map(),
-          answers: new Map(),
-          candidates: new Map()
-        };
-        ws.send(JSON.stringify({ type: "room-created", roomId }));
-        break;
-      case "join-room":
-        if (!rooms[data.roomId]) break;
-        rooms[data.roomId].guests.push(ws);
-        ws.send(JSON.stringify({ type: "room-joined", roomId: data.roomId }));
-        break;
-
-      case "offer":
-      case "answer":
-      case "candidate":
-        if (!rooms[data.roomId]) break;
-        const fieldType = `${data.type}s` as const;
-        (rooms[data.roomId][fieldType] as Map<WebSocket, typeof data.rtcData>).set(ws, data.rtcData);
-        break;
+    if (data.type === "host-room") {
+      const roomId = Math.random().toString(36).substr(2, 5);
+      rooms[roomId] = {
+        host: ws,
+        peers: new Map(),
+      };
+      ws.send(JSON.stringify({ type: "room-created", roomId }));
+    } 
+    else if (data.type === "join-room") {
+      if (!rooms[data.roomId]) return;
+      const peerId = generateUniquePeerId(rooms[data.roomId]);
+      rooms[data.roomId].peers.set(peerId, {
+        ws: ws,
+        offers: new Map(),
+        answers: new Map(),
+        candidates: new Map(),
+      });
+      ws.send(JSON.stringify({ type: "room-joined", roomId: data.roomId, peerId: peerId }));
+    }
+    else if (data.type === "offer" || data.type === "answer" || data.type === "candidate") {
+      if (!rooms[data.roomId] || !rooms[data.roomId].peers.has(data.rtcData.dst)) return;
+      const peerId = data.rtcData.dst;
+      const peer = rooms[data.roomId].peers.get(peerId)!;
+      const fieldType = `${data.type}s` as const;
+      (peer[fieldType] as Map<WebSocket, typeof data.rtcData>).set(ws, data.rtcData);
     }
 
     console.log(rooms);
@@ -99,12 +98,20 @@ wss.on("connection", (ws) => {
       if (rooms[roomId].host === ws) {
         delete rooms[roomId];
       } else {
-        rooms[roomId].guests = rooms[roomId].guests.filter(
-          (guest) => guest !== ws
-        );
+        for (const [peerId, peer] of rooms[roomId].peers) {
+          if (peer.ws === ws) {
+            rooms[roomId].peers.delete(peerId);
+          }
+        }
       }
     }
   });
 });
+
+const generateUniquePeerId = (room: Room): number => {
+  const peerId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+  return room.peers.has(peerId) ? generateUniquePeerId(room) : peerId;
+};
+
 
 console.log("WebRTC signaling server running on ws://localhost:8080");
